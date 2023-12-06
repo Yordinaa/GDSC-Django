709371bdc1ed5cb630c99d0d8cb47eb697f0fa7a
Yordinaa <105152673+Yordinaa@users.noreply.github.com>
2023-12-06 05:59:37 +0300

    Update print.py

--- week3/print.py
+++ week3/print.py
@@ -1,3 +1,12 @@
+"""week-3 task: Write a program that prints numbers from 0 to 99.
+- Numbers must be separated by ,, followed by a space
+- Numbers should be printed in ascending order, with two digits
+- The last number should be followed by a new line
+- You can only use no more than 2 print functions with string format
+- You can only use one loop in your code
+- You are not allowed to store numbers or strings in a variable
+- You are not allowed to import any module
+"""
 def format_number(n):
     if n < 10:
      return f"0{n},"
